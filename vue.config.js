699706612bf2b58702cb803b80dfaa0c78fd8e5f
NiloSquash <nvidalh@uoc.edu>
2021-01-08 19:28:02 +0100

    change

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,28 @@ module.exports = {
         prependData: `@import "@/styles/_variables.scss";`
       }
     }
+  },
+
+  pwa: {
+    name: 'vue-uocify',
+    themeColor: '#F2F9FA',
+    msTileColor: '#000001',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestPath: 'manifest.json',
+    manifestOptions: {
+      name: 'vue-uocify',
+      short_name: 'vue-uocify',
+      start_url: '.',
+      display: 'standalone',
+      theme_color: '#F2F9FA'
+    },
+  
+iconPaths: {
+  favicon: 'public/favicon.ico',
+},
+
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
   }
 };
